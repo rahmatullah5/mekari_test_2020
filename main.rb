9b7549adb4a16fafa826809458150f188613ae3a
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2020-03-23 21:33:28 +0700

    fix grammar

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ recite_line = gets
 puts 'Randomize value (y/n) ?'
 randomize = gets
 
-puts 'only subject from recited line (y/n) ?'
+puts 'only subject from the recited line (y/n) ?'
 subject_only = gets
 
 puts Reciter.new(recite_line, randomize, subject_only).call
