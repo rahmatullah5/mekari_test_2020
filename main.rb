d4580bec402d34d50720620a0f79314cbeb2465a
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2020-03-23 21:15:10 +0700

    init main.rb and its function

---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative  './reciter.rb'
+
+puts 'Recite Line :'
+recite_line = gets
+
+puts 'Randomize value (y/n) ?'
+randomize = gets
+
+puts 'only subject from recited line (y/n) ?'
+subject_only = gets
+
+puts Reciter.new(recite_line, randomize, subject_only).call
